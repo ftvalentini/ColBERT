aadb44e493a91e5a0bf481bf6255d87eb13adb18
Francisco Valentini <ft.valentini@gmail.com>
2023-08-07 17:07:37 -0300

    script to generate colbertv2 embeddings

---
+++ generate_passage_embeddings.py
@@ -0,0 +1,60 @@
+"""Encodes all passages into matrices, stores them on disk, and builds data 
+structures for efficient search.
+
+Sources: 
+* https://github.com/stanford-futuredata/ColBERT#indexing
+* https://github.com/stanford-futuredata/ColBERT/blob/d5b4a0453361ebcf09b9d62330ae471cf6cfe6dd/docs/intro.ipynb
+"""
+
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+
+from colbert.infra import Run, RunConfig, ColBERTConfig
+from colbert import Indexer
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter("[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s", "%m/%d/%Y %H:%M:%S")
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
+
+
+def main():
+
+    # args:
+    parser = ArgumentParser()
+    parser.add_argument("in_file", type=str, help="Path to input tsv file with chunks")
+    parser.add_argument("out_dir", type=str, help="Path to output directory")
+    parser.add_argument("--model_path", type=str, default="../../data/models/colbertv2.0",
+                        help="Path to ColBERT checkpoint")
+    parser.add_argument("--overwrite", action="store_true", 
+                        help="Overwrite index if it already exists")
+
+    args = parser.parse_args()
+
+    logger.info(f"Running ColBERTv2 indexing...")
+
+    with Run().context(RunConfig(nranks=1, experiment="msmarco")):
+        config = ColBERTConfig(
+            nbits=2, 
+            root=args.out_dir,
+        )
+        indexer = Indexer(checkpoint=args.model_path, config=config)
+        indexer.index(
+            name="prueba",
+            collection=args.in_file,
+            overwrite=args.overwrite,
+        )
+
+    # touch DONE file:
+    # Path(args.out_dir) / "DONE".touch()
+    Path(args.out_dir).parent.joinpath("DONE").touch()
+
+    logger.info(f"DONE!")
+
+
+if __name__=='__main__':
+    main()
