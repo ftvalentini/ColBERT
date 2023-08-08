e21d56bd9f27be18f0642770e3327209dfcb9686
Francisco Valentini <ft.valentini@gmail.com>
2023-08-08 16:13:23 -0300

    fix path structure of colbert output

--- generate_passage_embeddings.py
+++ generate_passage_embeddings.py
@@ -4,6 +4,7 @@ structures for efficient search.
 Sources: 
 * https://github.com/stanford-futuredata/ColBERT#indexing
 * https://github.com/stanford-futuredata/ColBERT/blob/d5b4a0453361ebcf09b9d62330ae471cf6cfe6dd/docs/intro.ipynb
+* scripts/ColBERT/colbert/tests/index_updater_test.py
 """
 
 import logging
@@ -37,21 +38,22 @@ def main():
 
     logger.info(f"Running ColBERTv2 indexing...")
 
-    with Run().context(RunConfig(nranks=1, experiment="msmarco")):
+    with Run().context(RunConfig(nranks=1, root=args.out_dir, index_root=args.out_dir)):
         config = ColBERTConfig(
+            doc_maxlen=1000, # Set to 1000 to avoid truncation TODO default is 220
             nbits=2, 
-            root=args.out_dir,
+            # experiment=None,
         )
         indexer = Indexer(checkpoint=args.model_path, config=config)
         indexer.index(
-            name="prueba",
+            name="default", # NOTE name is mandatory
             collection=args.in_file,
             overwrite=args.overwrite,
         )
 
     # touch DONE file:
-    # Path(args.out_dir) / "DONE".touch()
     Path(args.out_dir).parent.joinpath("DONE").touch()
+    # Path(args.out_dir) / "DONE".touch()
 
     logger.info(f"DONE!")
 
